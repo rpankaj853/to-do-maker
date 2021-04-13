c7ea26e0e8148a91d18e9fefa5dce0c8254cc1a3
pankajrana <rpankaj853@gmail.com>
2021-04-13 13:09:01 +0530

    my Second commit

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #auth
     path('signup/',views.signupuser,name='signupuser'),
     path('login/',views.loginuser,name='loginuser'),
    	path('logout/',views.logoutuser,name='logoutuser'),
+
+    #todos
     path('',views.home,name='home'),
     path('create/',views.createtodo,name='createtodo'),
     path('current/',views.currenttodos,name='currenttodos'),
@@ -29,6 +33,9 @@ urlpatterns = [
     path('todo/<int:todo_pk>',views.viewtodo,name='viewtodo'),
     path('todo/<int:todo_pk>/complete',views.completetodo,name='completetodo'),
     path('todo/<int:todo_pk>/delete',views.deletetodo,name='deletetodo'),
+
+    #api
+    path('api/',include('todoapi.urls')),
 
     
     ]
